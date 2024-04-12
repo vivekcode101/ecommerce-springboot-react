76156eb794f7d17beabca640f7234caef462c23c
Vivek Yadav <vyadav@initializ.io>
2024-04-12 22:58:33 +0530

    Buttons are implemented in the first page
    Signed-off-by: Vivek Yadav <vyadav@initializ.io>

---
+++ src/customer/components/HomeCarosel/MainCarosel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { maincaroseldata } from './MainCarselData'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
+const MainCarosel = () => {
+
+    const items = maincaroseldata.map((item)=> <img className='cursor-pointer' 
+    role='presentation' src={item.image} alt=""/>)
+
+  return (
+    <AliceCarousel
+    items={items}
+    disableButtonsControls
+    autoPlay
+    autoPlayInterval={1000}
+    infinite
+    />
+  )
+}
+
+export default MainCarosel
---
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import HomeSectionCard from '../HomeSectionCard/HomeSectionCard'
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { Button } from "@mui/material"
+
+const HomeSectionCarosel = () => {
+    const responsive = {
+        0: { items: 1 },
+        720: { items: 3 },
+        1024: { items: 4 },
+    }
+
+    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+
+    return (
+        <div className='relative px-4 lg:px-8'>
+            <div className='relative p-5'>    
+            <AliceCarousel
+                items={items}
+                disableButtonsControls
+                infinite
+                responsive={responsive}
+            />
+            <Button>
+            <KeyboardArrowLeftIcon/>
+            </Button>
+            </div>
+        </div>
+    )
+}
+
+export default HomeSectionCarosel
