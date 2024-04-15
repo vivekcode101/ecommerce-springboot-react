83a92bd6c14e19f9d5342d63bbb4ed8520e2be47
Vivek Yadav <vyadav@initializ.io>
2024-04-15 11:11:57 +0530

    Added buttons
    Signed-off-by: Vivek Yadav <vyadav@initializ.io>

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -14,17 +14,25 @@ const HomeSectionCarosel = () => {
     const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
 
     return (
-        <div className='relative px-4 lg:px-8'>
+        <div className='px-4 lg:px-8'>
             <div className='relative p-5'>    
             <AliceCarousel
                 items={items}
                 disableButtonsControls
                 infinite
+                disableDotsControls 
                 responsive={responsive}
             />
-            <Button>
-            <KeyboardArrowLeftIcon/>
+            <Button variant ="contained"className='z-50 bg-white' sx={{position:'absolute', top:'8rem', right:"0rem"
+                ,transform:"translateX(50%) rotate(90deg)" ,bgcolor:'white'}} aria-label='next'  >
+            <KeyboardArrowLeftIcon sx={{transform:'rotate(90deg)',color:'black'}}/>
             </Button>
+
+            <Button variant ="contained"className='z-50 bg-white' sx={{position:'absolute', top:'8rem', leftt:"0rem"
+                ,transform:"translateX(50%) rotate(90deg)" ,bgcolor:'white'}} aria-label='next'  >
+            <KeyboardArrowLeftIcon sx={{transform:'rotate(-90deg)',color:'black'}}/>
+            </Button>
+
             </div>
         </div>
     )
