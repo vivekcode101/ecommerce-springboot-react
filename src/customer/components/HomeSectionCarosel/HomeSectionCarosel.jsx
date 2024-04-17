cbb3eaa7e1b913c10796d245ba81e3db2f6a1015
Vivek Yadav <vyadav@initializ.io>
2024-04-17 12:35:07 +0530

    imported data slide is not changing
    Signed-off-by: Vivek Yadav <vyadav@initializ.io>

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,41 +1,76 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard'
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import { Button } from "@mui/material"
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { Button } from "@mui/material";
+import { mens_kurta } from "../../../Data/mens_kurta";
 
 const HomeSectionCarosel = () => {
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 4 },
-    }
+    const [activeIndex,setActiveIndex] = useState(0);
 
-    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
 
-    return (
-        <div className='px-4 lg:px-8'>
-            <div className='relative p-5'>    
-            <AliceCarousel
-                items={items}
-                disableButtonsControls
-                infinite
-                disableDotsControls 
-                responsive={responsive}
-            />
-            <Button variant ="contained"className='z-50 bg-white' sx={{position:'absolute', top:'8rem', right:"0rem"
-                ,transform:"translateX(50%) rotate(90deg)" ,bgcolor:'white'}} aria-label='next'  >
-            <KeyboardArrowLeftIcon sx={{transform:'rotate(90deg)',color:'black'}}/>
-            </Button>
+  const slidePrev=()=>setActiveIndex(activeIndex-1);
+  const slideNext=()=>setActiveIndex(activeIndex+1);
 
-            <Button variant ="contained"className='z-50 bg-white' sx={{position:'absolute', top:'8rem', leftt:"0rem"
-                ,transform:"translateX(50%) rotate(90deg)" ,bgcolor:'white'}} aria-label='next'  >
-            <KeyboardArrowLeftIcon sx={{transform:'rotate(-90deg)',color:'black'}}/>
-            </Button>
+  const syncActiveIndex=({item})=>setActiveIndex(item);
 
-            </div>
-        </div>
-    )
-}
+  const items = mens_kurta.slice(0,10).map((item) => <HomeSectionCard  product={item} />);
 
-export default HomeSectionCarosel
+  return (
+    <div className="border">
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls          
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        {activeIndex!==items.length-5 &&  <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slideNext}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>}
+
+        <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slidePrev}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            leftt: "0rem",
+            transform: "translateX(-50%) rotate(-90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
